e86a0af7c36c32ee9098102ecd089927afb21239
pateldipal <pateldipal170419@gmail.com>
2026-02-05 21:04:24 +0530

    all new file add

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, dolore? Ea, 
+        laboriosam officiis nemo repellat assumenda aperiam temporibus.</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/header.jsx
@@ -0,0 +1,8 @@
+function Header(){
+    return <div className="div">
+        <h1>header file here.....</h1>
+        <div>hello</div> 
+        <img src="" alt="" />
+    </div>
+} 
+export default Header;
