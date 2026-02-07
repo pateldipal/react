124265119429f98ba90702e41a3b043a0c05b3bd
pateldipal <pateldipal170419@gmail.com>
2026-02-07 13:35:14 +0530

    card design

---
+++ Component_Card/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Cardprops } from './component/Cardprops'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <Cardprops />
+    </div>
+  )
+}
+
+export default App
---
+++ Component_Card/src/component/Cardprops.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Bookmark } from 'lucide-react';
+
+export const Cardprops = () => {
+  return (
+    <div className='parent'>
+        <div className='card'>
+       <div>
+         <div className='top'>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa9Mdeo4S4YXDOaI4Xm53DaaHVlccVG_j7Yg&s" alt="" />
+            <button>Save <Bookmark size={12}/> </button> 
+        </div>
+        <div className='center'>
+            <h3>Amezon <span> Five Days ago</span></h3>
+            <h2>Senior UI/UX Designer</h2>
+           <div className='tag'>
+                <h4>Part Time</h4>
+                <h4>Senior Level</h4> 
+            </div> 
+        </div>
+       </div>
+        <div className='bottom'>
+            <div>
+                <h3>$120/hr</h3>
+                <p>Mumbai,India</p>
+            </div>
+            <button>Apply Now</button>
+        </div>
+    </div>
+    
+    </div>
+   
+
+  )
+}
